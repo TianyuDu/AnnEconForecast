3291649c71c341c1b2b2dfc4a4a11b0aec1462b4
EC2 Default User <ec2-user@ip-172-31-89-216.ec2.internal>
2018-11-04 23:13:36 +0000

    Update constants

--- constants.py
+++ constants.py
@@ -6,5 +6,5 @@ Constants.
 # Directories of UNRATE.csv file under different environemnt.
 UNRATE_DIR = {
     "MAC": "/Users/tianyudu/Documents/Academics/EconForecasting/EFANN/data/UNRATE.csv",
-    "EC2": "/home/ec2-user/code/EFANN/data"
+    "EC2": "/home/ec2-user/code/EFANN/data/UNRATE.csv"
 }
