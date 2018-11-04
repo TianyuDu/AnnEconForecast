789183337ec54dfbc196c6c3fb9470724c3f1dc8
MasterDoo <masterdu9951@icloud.com>
2018-11-04 10:39:14 -0800

    add load dataset

--- data/core/tools/data_import.py
+++ data/core/tools/data_import.py
@@ -5,8 +5,14 @@ reading raw data into data frames.
 
 import numpy as np
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
 
 from constants import UNRATE_DIR
 
-
-
+def load_dataset(
+    dir: str,
+) -> pd.DataFrame:
+    df = pd.read_csv(dir, sep=",", header="infer", index_col=0)
+    print(f"Dataset loaded, with type {df.values.dtype}.")
+    return df
