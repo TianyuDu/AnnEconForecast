9632a1a16bdeec17654a7b47b30288b07a4d2369
MasterDoo <masterdu9951@icloud.com>
2018-11-04 12:48:31 -0800

    update differencing method

--- data/core/tools/time_series.py
+++ data/core/tools/time_series.py
@@ -9,13 +9,22 @@ import pandas as pd
 
 def differencing(
     src_df = pd.DataFrame,
-    order: int=1
+    order: int=1,
+    periods: int=1
 ) -> pd.DataFrame:
-    pass
+    df = src_df.copy()
+    
+    for od in range(order):
+        df = df.diff(periods=periods)
+
+    new_cols = [col+f"_d{periods}_x{order}" for col in df.columns]
+    df.columns = new_cols
+    return df
 
 
 def invert_diff(
     src_df = pd.DataFrame,
-    order: int=1
+    order: int=1,
+    periods: int=1
 ) -> pd.DataFrame:
     pass
--- playground.py
+++ playground.py
@@ -1,3 +1,6 @@
 from constants import *
+from core.tools import *
 
 df = load_dataset(UNRATE_DIR["MAC"])
+df_dd = differencing(df, periods=1, order=1)
+df_d1 = differencing(df, periods=1, order=2)
