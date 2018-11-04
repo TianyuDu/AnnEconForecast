53ea3a59c9fa7f6852b5772686cddd30062025ce
MasterDoo <masterdu9951@icloud.com>
2018-11-04 10:51:40 -0800

    update time series package

--- data/core/tools/time_series.py
+++ data/core/tools/time_series.py
@@ -7,5 +7,15 @@ import numpy as np
 import pandas as pd
 
 
-def differencing():
-    
+def differencing(
+    src_df = pd.DataFrame,
+    order: int=1
+) -> pd.DataFrame:
+    pass
+
+
+def invert_diff(
+    src_df = pd.DataFrame,
+    order: int=1
+) -> pd.DataFrame:
+    pass
