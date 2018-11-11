5194707160191df1df7a740c41189a3032391ed7
Ubuntu <ubuntu@ip-172-31-50-197.ec2.internal>
2018-11-11 09:29:51 +0000

    update

--- constants.py
+++ constants.py
@@ -6,5 +6,6 @@ Constants.
 # Directories of UNRATE.csv file under different environemnt.
 UNRATE_DIR = {
     "MAC": "/Users/tianyudu/Documents/Academics/EconForecasting/AnnEconForecast/data/UNRATE.csv",
-    "EC2": "/home/ec2-user/code/AnnEconForecast/data/UNRATE.csv"
+    "EC2": "/home/ec2-user/code/AnnEconForecast/data/UNRATE.csv",
+    "EC2GPU": "/home/ubuntu/AnnEconForecast/data/UNRATE.csv"
 }
