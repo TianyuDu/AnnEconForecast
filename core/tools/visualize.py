7137a7fca109a2565c9120acb5e9f15c71ef75ae
MasterDoo <masterdu9951@icloud.com>
2018-11-11 00:46:30 -0500

    add ref to visualize

--- core/tools/visualize.py
+++ core/tools/visualize.py
@@ -4,6 +4,10 @@ Visualization tools.
 
 
 def progbar(curr, total, full_progbar):
+    """
+    Progress bar used in training process.
+    Reference: https://geekyisawesome.blogspot.com/2016/07/python-console-progress-bar-using-b-and.html
+    """
     frac = curr/total
     filled_progbar = round(frac*full_progbar)
     print('\r', '#'*filled_progbar + '-'*(
