b2b1a5d83adb8e4d01443fbeca72b17ab5cf83c3
EC2 Default User <ec2-user@ip-172-31-21-35.ec2.internal>
2018-11-17 05:12:02 +0000

    update constant

--- constants.py
+++ constants.py
@@ -8,5 +8,5 @@ DATA_DIR = {
     "0": "/Users/tianyudu/Documents/Academics/EconForecasting/AnnEconForecast/data/UNRATE.csv",
     "1": "/home/ec2-user/code/AnnEconForecast/data/UNRATE.csv",
     "2": "/home/ubuntu/AnnEconForecast/data/UNRATE.csv",
-    "3": "/home/ubuntu/AnnEconForecast/data/DEXCAUS.csv"
+    "3": "/home/ec2-user/AnnEconForecast/data/DEXCAUS.csv"
 }
