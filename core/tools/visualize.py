a5957a09a6cf021ea4b11f90ac45f2a3e1e5d892
MasterDoo <masterdu9951@icloud.com>
2018-11-26 22:00:28 -0500

    add plot_checkpoints

--- core/tools/visualize.py
+++ core/tools/visualize.py
@@ -1,6 +1,10 @@
 """
 Visualization tools.
 """
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from typing import Dict
 
 
 def progbar(curr, total, full_progbar):
@@ -12,3 +16,24 @@ def progbar(curr, total, full_progbar):
     filled_progbar = round(frac*full_progbar)
     print('\r', '#'*filled_progbar + '-'*(
         full_progbar-filled_progbar), '[{:>7.2%}]'.format(frac), end='')
+
+
+def plot_checkpoints(
+    predictions: Dict[int, Dict[str, np.ndarray]],
+    actual: np.ndarray,
+    key: str
+):
+    #     width = np.ceil(np.sqrt(len(predictions))).astype(int)
+    f, axarr = plt.subplots(
+        len(predictions.keys()),
+        sharex=False,
+        sharey=False,
+        figsize=(32, 8*len(predictions.keys())))
+    for i, e in enumerate(predictions.keys()):
+        pred = predictions[e]
+        axarr[i].plot(pred[key].reshape(-1, 1), alpha=0.6)
+        axarr[i].plot(actual.reshape(-1, 1), alpha=0.6)
+        axarr[i].set_title(f"{key} set prediction after [{e}] epochs.")
+        axarr[i].legend([f"{key} prediction", f"{key} actual"])
+        axarr[i].grid(True)
+    return f, axarr
