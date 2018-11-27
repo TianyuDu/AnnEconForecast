01e5c449e2075ca3119a742e303d3123a195b817
MasterDoo <masterdu9951@icloud.com>
2018-11-26 22:56:47 -0500

    add docs and verbose for prepare_dataset

--- core/tools/rnn_prepare.py
+++ core/tools/rnn_prepare.py
@@ -2,6 +2,7 @@
 This file contains data preparation methods for RNN.
 """
 import sys
+import os.path
 from pprint import pprint
 from typing import Dict, Union, Iterable
 
@@ -17,26 +18,72 @@ from sklearn.preprocessing import StandardScaler
 from constants import *
 from core.models.baseline_rnn import *
 from core.models.stat_models import *
-from core.tools.data_import import *
+import core.tools.data_import as data_import
 from core.tools.metrics import *
-from core.tools.time_series import *
-from core.tools.visualize import *
+import core.tools.time_series as time_series
 
 sys.path.extend(["../"])
 
 
 def prepare_dataset(
     file_dir: str,
-    periods: int = 1,
-    order: int = 1
+    periods: int=1,
+    order: int=1,
+    verbose: bool=True
 ) -> pd.DataFrame:
-    df = load_dataset(file_dir)
-    prepared_df = differencing(df, periods=periods, order=order)
+    """
+    Prepare the dataset for RNN Training.
+
+    Args:
+        file_dir:
+            A string locating the directory of *.csv file to be loaded.
+        periods:
+            An integer representing the number of periods of looking back
+            when taking the difference on raw dataset.
+        order:
+            An integer representing the (recursive) times of differencing is
+            computed on the raw dataset.
+            * Analogue to the "d" parameter in ARIMA(p,d,q)
+
+        ** In most cases, use periods=1 and order=1 should be sufficient.
+
+        verbose:
+            A bool indicating whether report should be printed during the process.
+
+    Returns:
+        A dataframe of prepared dataset.
+    """
+    # ======== Args Check ========
+    assert os.path.exists(file_dir), f"File {file_dir} cannot be found."
+
+    assert isinstance(periods, int), "Periods arg should be an integer."
+    assert periods >= 1, "Periods arg should be at least 1."
+
+    assert isinstance(order, int), "Order arg should be an integer."
+    assert order >= 1, "Order arg should be at least 1."
+
+    assert isinstance(verbose, bool), "Verbose arg should be a bool."
+
+    # ======== Core ========
+    if verbose:
+        print(f"Retrieving raw data from {file_dir}...")
+
+    df = data_import.load_dataset(file_dir)
+    if verbose:
+        print(f"Processing data, taking (periods, order)=({periods}, {order})...")
+    prepared_df = time_series.differencing(
+        df,
+        periods=periods,
+        order=order
+    )
     prepared_df.head()
+    if verbose:
+        print("Dropping Nan observations...")
     prepared_df.dropna(inplace=True)
 
-    print("First few rows of dataset loaded:")
-    print(prepared_df.head())
+    if verbose:
+        print("First few rows of dataset loaded:")
+        print(prepared_df.head())
     return prepared_df
 
 
