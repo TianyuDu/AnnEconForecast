7cb43a746fdd9e02956d45ec7ccb9f14ad7e9027
MasterDoo <masterdu9951@icloud.com>
2018-11-29 12:17:57 -0500

    add example arg to gen_hparam_set

--- core/training/hps_methods.py
+++ core/training/hps_methods.py
@@ -30,8 +30,26 @@ def gen_hparam_set(
         A list (iterable) with all combination of candidates in 
             flexiable (to be searched) parameters.
     """
-    # ======== EXAMPLE INPUT ========
 
+    # ======== EXAMPLE INPUT ========
+    parameters = {
+        "epochs": 1500,
+        "num_time_steps": [6, 12, 24],
+        "num_inputs": 1,
+        "num_outputs": 1,
+        "num_neurons": [
+            (256, 128),
+            (256, 128, 64),
+            (512, 256),
+            (512, 256, 128),
+            (1024, 512),
+            (1024, 512, 256)
+        ],
+        "learning_rate": [0.3, 0.1, 0.03],
+        "report_periods": 10,
+        "tensorboard_dir": "~/Desktop/tb/",
+        "model_path": "~/Desktop/saved_models/"
+    }
     # ======== END ========    
     gen = list()
     detected_list_keys = list()
