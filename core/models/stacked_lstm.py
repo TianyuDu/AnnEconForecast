65c5b42fcd2a048dc4f9a792bb0dac21e30054d6
MasterDoo <masterdu9951@icloud.com>
2018-12-17 19:00:08 -0500

    update exec_core and add make_prediction_all

--- core/models/stacked_lstm.py
+++ core/models/stacked_lstm.py
@@ -25,24 +25,44 @@ from core.tools.visualize import *
 
 # sys.path.extend(["/Users/tianyudu/Documents/Academics/EconForecasting/AnnEconForecast"])
 
+def make_prediction_all(
+    pred: tf.Tensor,
+    X: tf.Tensor,
+    data: Dict[str, np.ndarray]
+) -> Dict[str, np.ndarray]:
+    p_train = pred.eval(feed_dict={X: data["X_train"]})
+    p_test = pred.eval(feed_dict={X: data["X_test"]})
+    p_val = pred.eval(feed_dict={X: data["X_val"]})
+    collection = {
+        "train": p_train,
+        "test": p_test,
+        "val": p_val
+    }
+    return collection
+
 
 def exec_core(
-    parameters: Dict[str, object],
-    data_collection: Dict[str, np.ndarray],
+    param: Dict[str, object],
+    data: Dict[str, np.ndarray],
     prediction_checkpoints: Iterable[int]=[-1],
     verbose: bool=False
 ) -> Tuple[
         Dict[str, float],
         Dict[int, Dict[str, np.ndarray]]
 ]:
+    """
+    # TODO: write the doc string.
+    """
     if verbose:
         print("Resetting Tensorflow defalut graph...")
     tf.reset_default_graph()
 
-    globals().update(parameters)
-    globals().update(data_collection)
+    # TODO: remove the update method
+    # Ue 
 
-    predictions = dict()
+    # globals().update(param)
+    # globals().update(data)
+
     assert all(isinstance(x, int)
                for x in prediction_checkpoints), "Invalid checkpoint of recording."
     assert all(
@@ -51,34 +71,55 @@ def exec_core(
     with tf.name_scope("DATA_FEED"):
         X = tf.placeholder(
             tf.float32,
-            [None, num_time_steps, num_inputs],
-            name="Feature_X"
+            [None, param["num_time_steps"], param["num_inputs"]],
+            name="FEATURE"
         )
         y = tf.placeholder(
             tf.float32,
-            [None, num_outputs],
-            name="Label_y"
+            [None, param["num_outputs"]],
+            name="LABEL"
         )
 
     # with tf.name_scope("RECURRENT_UNITS"):
     multi_cell = tf.nn.rnn_cell.MultiRNNCell(
         [tf.nn.rnn_cell.LSTMCell(
-            num_units=x,
-            name=f"LSTM_Cell_{i}"
+            num_units=units,
+            name=f"LSTM_CELL_{i}"
             )
-        for i, x in enumerate(num_neurons)]
+        for i, units in enumerate(param["num_neurons"])
+        ]
     )
 
-    rnn_outputs, states = tf.nn.dynamic_rnn(multi_cell, X, dtype=tf.float32)
+    rnn_outputs, states = tf.nn.dynamic_rnn(
+        multi_cell,
+        X,
+        dtype=tf.float32
+    )
+
     stacked_output = tf.reshape(
-        rnn_outputs, [-1, num_time_steps * num_neurons[-1]])
+        rnn_outputs,
+        [-1, param["num_time_steps"] * param["num_neurons"][-1]]
+    )
 
     with tf.name_scope("OUTPUT_LAYER"):
-        W = tf.Variable(tf.random_normal(
-            [num_time_steps * num_neurons[-1], 1]), dtype=tf.float32, name="Weight")
-        b = tf.Variable(tf.random_normal([1]), dtype=tf.float32, name="Bias")
+        W = tf.Variable(
+            tf.random_normal(
+                [param["num_time_steps"] * param["num_neurons"][-1], 1]
+            ),
+            dtype=tf.float32,
+            name="WEIGHT"
+        )
 
-        pred = tf.add(tf.matmul(stacked_output, W), b, name="Prediction")
+        b = tf.Variable(
+            tf.random_normal([1]),
+            dtype=tf.float32,
+            name="BIAS"
+        )
+
+        pred = tf.add(
+            tf.matmul(stacked_output, W), b,
+            name="PREDICTION"
+        )
 
         tf.summary.histogram("weights", W)
         tf.summary.histogram("biases", b)
@@ -87,83 +128,102 @@ def exec_core(
     # pred = tf.layers.dense(stacked_output, 1)
 
     with tf.name_scope("METRICS"):
-        loss = tf.reduce_mean(tf.square(y - pred), name="mse")
-
+        # MSE is the main loss we focus on and it's the metric used for optimization.
+        loss = tf.reduce_mean(tf.square(y - pred), name="MSE")
         mape = tf.reduce_mean(tf.abs(tf.divide(y - pred, y)))
-        tf.summary.scalar("mean_squared_error", loss)
-        tf.summary.scalar("mean_absolute_percentage_error", mape)
+
+        tf.summary.scalar("MSE", loss)
+        tf.summary.scalar("MAPE", mape)
 
     with tf.name_scope("OPTIMIZER"):
         optimizer = tf.train.AdamOptimizer(
-            learning_rate=learning_rate, name="Adam_optimizer")
+            learning_rate=param["learning_rate"],
+            name="ADAM_OPTIMIZER"
+        )
 
-        if clip_grad is None:
+        if param["clip_grad"] is None:
+            # No Gradient Clipping
             if verbose:
                 print("Note: no gradient clipping is applied.\
                 \nIf possible gradient exploding detected (e.g. nan loss), try use clip_grad.")
             train = optimizer.minimize(loss)
         else:
+            # With Gradient Clipping
             if verbose:
                 print("Applying gradient clipping...")
-                print(f"\tClip by values: {clip_grad}")
+                print(f"\tClip by values: {param['clip_grad']}")
             gvs = optimizer.compute_gradients(loss)
             capped_gvs = [
-                (tf.clip_by_value(grad, - clip_grad, clip_grad), var)
+                (tf.clip_by_value(
+                    grad, - param["clip_grad"], param["clip_grad"]), var)
                 for grad, var in gvs
             ]
             train = optimizer.apply_gradients(capped_gvs)
 
-    # X_batches = X_train[:-84].reshape(100, -1, num_time_steps, num_inputs)
-    # y_batches = y_train[:-84].reshape(100, -1, num_outputs)
-    # print(X_batches.shape)
-    # print(y_batches.shape)
 
-    tb_dir = tensorboard_dir
+    # TODO: remove this
+    tb_dir = param["tensorboard_dir"]
 
     start = datetime.now()
-    # hist = {"train": [], "val": []}
+    
+    predictions = dict()
+    if verbose:
+        print("Running training session...")
+
     with tf.Session() as sess:
         saver = tf.train.Saver()
         merged_summary = tf.summary.merge_all()
-        train_writer = tf.summary.FileWriter(tb_dir + "/train")
-        val_writer = tf.summary.FileWriter(tb_dir + "/validation")
+        
+        train_writer = tf.summary.FileWriter(
+            param["tensorboard_dir"] + "/train")
+
+        val_writer = tf.summary.FileWriter(
+            param["tensorboard_dir"] + "/validation")
         train_writer.add_graph(sess.graph)
+
         # val_writer.add_graph(sess.graph)
 
         sess.run(tf.global_variables_initializer())
-        for e in range(epochs):
-            #         for X_batch, y_batch in zip(X_batches, y_batches):
-            #             sess.run(train, feed_dict={X: X_batch, y: y_batch})
-            sess.run(train, feed_dict={X: X_train, y: y_train})
-            train_mse = loss.eval(feed_dict={X: X_train, y: y_train})
-            val_mse = loss.eval(feed_dict={X: X_val, y: y_val})
-            # hist["train"].append(train_mse)
-            # hist["val"].append(val_mse)
+        for e in range(param["epochs"]):
+            # Use this if train with batches
+            # for X_batch, y_batch in zip(X_batches, y_batches):
+            #     sess.run(train, feed_dict={X: X_batch, y: y_batch})
+
+            sess.run(train, feed_dict={X: data["X_train"], y: data["y_train"]})
+
             if e % report_periods == 0:
-                s_train = sess.run(merged_summary, feed_dict={
-                    X: X_train, y: y_train})
-                s_val = sess.run(merged_summary, feed_dict={
-                                 X: X_val, y: y_val})
-                train_writer.add_summary(s_train, e)
-                val_writer.add_summary(s_val, e)
-            if e % (report_periods * 10) == 0:
-                # print 10 times less frequently than the record frequency.
+                # In those periods, training summary is written to tensorboard record.
+                # Summary on training set.
+                train_summary = sess.run(
+                    merged_summary,
+                    feed_dict={X: data["X_train"], y: data["y_train"]}
+                )
+                # Summary on validation set.
+                val_summary = sess.run(
+                    merged_summary,
+                    feed_dict={X: data["X_val"], y: data["y_val"]}
+                )
+
+                train_writer.add_summary(train_summary, e)
+                val_writer.add_summary(val_summary, e)
+
+            if e % (report_periods * 10) == 0 and verbose:
+                # print out training result 10 times less frequently than the record frequency.
+                train_mse = loss.eval(
+                    feed_dict={X: data["X_train"], y: data["y_train"]}
+                )
+                val_mse = loss.eval(
+                    feed_dict={X: data["X_val"], y: data["y_val"]}
+                )
+
                 print(
                     f"\nIteration [{e}], Training MSE {train_mse:0.7f}; Validation MSE {val_mse:0.7f}")
+            
+
             if e in prediction_checkpoints:
-                p_train = pred.eval(feed_dict={X: X_train})
-                p_test = pred.eval(feed_dict={X: X_test})
-                p_val = pred.eval(feed_dict={X: X_val})
-                predictions[e] = {
-                    "train": p_train,
-                    "test": p_test,
-                    "val": p_val
-                }
+                predictions[e] = make_prediction_all(pred, X, data)
 
         if -1 in prediction_checkpoints:
-            p_train = pred.eval(feed_dict={X: X_train})
-            p_test = pred.eval(feed_dict={X: X_test})
-            p_val = pred.eval(feed_dict={X: X_val})
             predictions[epochs] = {
                 "train": p_train,
                 "test": p_test,
