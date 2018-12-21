fada9e75020735e2a36e328db34d7d3b0fb722a6
ec2 <i@tianyudu.com>
2018-12-21 19:18:10 +0000

    update hps

--- hps_configs/ec2_config.py
+++ hps_configs/ec2_config.py
@@ -5,7 +5,7 @@ Default hyper parameter searching configuration working on EC2.
 EXPERIMENT_NAME = "ec2_large"
 
 # Model training parameters
-MAIN_DIRECTORY = "/home/ec2-user/ec2_hps/2018DEC20_02"
+MAIN_DIRECTORY = "/home/ec2-user/ec2_hps/2018DEC21_01"
 main = {
     # ======== Data Pre-processing Parameter ========
     "PERIODS": 1,
@@ -14,24 +14,24 @@ main = {
     "TRAIN_RATIO": 0.8,
     "VAL_RATIO": 0.1,
     # ======== Model Training Parameter ========
-    "epochs": [150, 300, 500, 800],
+    "epochs": [150, 300, 500, 1000],
     "num_inputs": 1,
     "num_outputs": 1,
     "num_time_steps": None,  # num_time_steps is identical to LAGS
     "num_neurons": [
-        (64, 128),
         (128, 256),
         (256, 512),
-        (64, 128, 256),
+        (512, 1024),
         (128, 256, 512),
-        (256, 512, 1024)
+        (256, 512, 1024),
+        (512, 1024, 2048)
     ],
     "learning_rate": [
+        0.003,
         0.01,
-        0.03,
-        0.1
+        0.03
     ],
-    "clip_grad": None,
+    "clip_grad": [None, 10, 50],
     "report_periods": 10,
     "tensorboard_path": MAIN_DIRECTORY + "/tensorboard/",
     "model_path": MAIN_DIRECTORY + "/saved_models/",
