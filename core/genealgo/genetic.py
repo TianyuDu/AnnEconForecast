d448abbcedc56ad69e8f78edc23c6faa9ce52f66
MasterDoo <masterdu9951@icloud.com>
2018-12-24 14:18:53 -0500

    add objects in ga

--- core/genealgo/genetic.py
+++ core/genealgo/genetic.py
@@ -2,3 +2,10 @@
 Dec 24 2018
 The base package containing the genetic algorithm packages.
 """
+
+class GenericGeneticOptimizer:
+    raise NotImplementedError
+
+
+class GeneticHyperParameterTuner(GenericGeneticOptimizer):
+    raise NotImplementedError
