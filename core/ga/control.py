beb79d08f32268a4bdae1c63ce34dc92645718fb
MasterDoo <masterdu9951@icloud.com>
2018-12-25 23:22:40 -0500

    update control

--- core/ga/control.py
+++ core/ga/control.py
@@ -1,8 +1,12 @@
 """
 The controller and experiment runner for genetic optimizer.
 """
+import numpy as np
+from typing import Dict
 from core.ga.genetic import GeneticOptimizer as GGO
 
 def obj_func(
-    param
-)
+    param: Dict[str, object]
+) -> float:
+    x = param["x"]
+    return 2 * x ** 2 + 3 * x + 4
