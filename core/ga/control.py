09fc9121134224f7a2e5cca568320790c1265897
MasterDoo <masterdu9951@icloud.com>
2018-12-26 02:28:42 -0500

    add demo to the genetic algorithm optimizer

--- core/ga/control.py
+++ core/ga/control.py
@@ -4,18 +4,19 @@ The controller and experiment runner for genetic optimizer.
 import numpy as np
 from typing import Dict
 from core.ga.genetic import GeneticOptimizer
+from matplotlib import pyplot as plt
 
 def obj_func(
     param: Dict[str, object]
 ) -> float:
     x = param["x"]
-    return 2 * x ** 2 + 3 * x + 4
+    return 2 * x ** 8 + 3 * x + 4
 
 # Searching range
-lb = -10
-ub = 10
-init_size = 100
-epochs = 30
+lb = -10.0
+ub = 10.0
+init_size = 500
+epochs = 20
 
 candidates = (ub - lb) * np.random.random(init_size) + lb
 
@@ -33,4 +34,16 @@ optimizer = GeneticOptimizer(
 for _ in range(epochs):
     optimizer.evaluation()
     optimizer.select(verbose=True)
+    print(optimizer.count_population())
     optimizer.evolve()
+
+ranked_minimizers = [entity["x"] for entity in optimizer.population]
+ranked_values = [obj_func(entity) for entity in optimizer.population]
+# plt.plot(ranked_values)
+# plt.show()
+
+# plt.plot(ranked_values)
+# plt.show()
+
+print(f"Optimizer x-star found at {optimizer.population[0]['x']:0.7f} \
+extremal value attained at {obj_func(optimizer.population[0]):0.7f}")
