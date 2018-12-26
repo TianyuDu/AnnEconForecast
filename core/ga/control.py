4f932ec6d6f4df640354105c5d6e52c0213cc9fc
MasterDoo <masterdu9951@icloud.com>
2018-12-26 02:40:38 -0500

    update control

--- core/ga/control.py
+++ core/ga/control.py
@@ -1,6 +1,8 @@
 """
 The controller and experiment runner for genetic optimizer.
 """
+import sys
+sys.path.append("./")
 import numpy as np
 from typing import Dict
 from core.ga.genetic import GeneticOptimizer
@@ -10,13 +12,14 @@ def obj_func(
     param: Dict[str, object]
 ) -> float:
     x = param["x"]
-    return 2 * x ** 8 + 3 * x + 4
+    f = - (2*x**7 + 923123213*x**12 + 3*x + 21312)
+    return f
 
 # Searching range
-lb = -10.0
-ub = 10.0
-init_size = 500
-epochs = 20
+lb = 1e10
+ub = -1e10
+init_size = 1000
+epochs = 200
 
 candidates = (ub - lb) * np.random.random(init_size) + lb
 
@@ -26,7 +29,7 @@ optimizer = GeneticOptimizer(
     gene_pool=gene_pool,
     pop_size=init_size,
     eval_func=obj_func,
-    mode="min",
+    mode="max",
     retain=0.5,
     verbose=True
 )
@@ -45,5 +48,8 @@ ranked_values = [obj_func(entity) for entity in optimizer.population]
 # plt.plot(ranked_values)
 # plt.show()
 
-print(f"Optimizer x-star found at {optimizer.population[0]['x']:0.7f} \
-extremal value attained at {obj_func(optimizer.population[0]):0.7f}")
+print(f"Optimizer x-star found at {optimizer.population[0]['x']:0.5f}")
+print(f"extremal value attained at {obj_func(optimizer.population[0]):0.5f}")
+
+# print("More attentions are required if the maximizer/minimizer is near boundary.")
+
