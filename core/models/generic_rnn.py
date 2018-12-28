273c243dcd9232bcd7be72d6b22a6d8d1c0f7b54
MasterDoo <masterdu9951@icloud.com>
2018-12-28 16:07:25 -0500

    add GenericRNN

--- core/models/generic_rnn.py
+++ core/models/generic_rnn.py
@@ -1,37 +1,67 @@
 """
-This file contains the generic recurrent neural network type.
+The baseline object for the general recurrent neural network
+object in the OOP version of this project.
 """
-import sys
-from pprint import pprint
-from typing import Dict, Iterable, Union
+from typing import Dict, Iterable, List, Union
 
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import sklearn
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-
-from constants import *
-from core.models.baseline_rnn import *
-from core.models.stat_models import *
-from core.tools.data_import import *
-import core.tools.metrics as metrics
-from core.tools.rnn_prepare import *
-from core.tools.time_series import *
-from core.tools.visualize import *
 
 
 class GenericRNN:
     """
     The generic recurrent network.
+    This is the abstract class used as the base of any recurrent 
+    neural network.
     """
+
     def __init__(
         self,
         param: Dict[str, object],
-        prediction_checkpoints: Iterable[int]=[-1],
-        verbose: bool=True
-    ) -> Nones:
+        prediction_checkpoints: Iterable[int] = [-1],
+        verbose: bool = True
+    ) -> None:
+        assert isinstance(param, dict),\
+        "Parameter set should be a dict."
+
+        assert all(isinstance(key, str) for key in param.keys()),\
+        "All keys in parameter set should be string."
+
+        assert all(isinstance(x, int) for x in prediction_checkpoints),\
+        "Invalid recording checkpoint, all elements should be integers."
+
+        assert all(-1 <= x <= param["epochs"] for x in prediction_checkpoints),\
+        "Some element(s) in checkpoint are out of range."
+
+        # Admit arguments.
+        self.param = param
+        self.param["num_time_steps"] = param["LAGS"]
+        self.ckpts = prediction_checkpoints
+    
+    def build(self) -> None:
+        """
+        Build up the neural network.
+        """
+        raise NotImplementedError()
+
+    def train(
+        self,
+        data: Dict[str, np.ndarray],
+        evaluate: Union[None, str, List[str]] = None
+    ) -> Union[None, Dict[str, float]]:
+        """
+        Train the neural network specified in this object.
+        """
+        raise NotImplementedError()
+    
+    def exec_core(
+        self,
+        param: Dict[str, object],
+        data: Dict[str, np.ndarray],
+        prediction_checkpoints: Iterable[int] = [-1],
+        verbose: bool = True
+    ) -> Dict[int, Dict[str, np.ndarray]]:
+        """
+        The non-OOP fashion exec_core method inherited from the 
+        eariler version.
+        """
         raise NotImplementedError()
