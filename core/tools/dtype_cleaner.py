7b0d32c92e471c867080dd9caa58cf51ae239db6
MasterDoo <masterdu9951@icloud.com>
2018-12-31 16:49:23 -0500

    add cleaner

---
+++ core/tools/dtype_cleaner.py
@@ -0,0 +1,30 @@
+"""
+Convert numpy data type to basic python data type.
+"""
+import numpy as np
+
+def clean(d) -> dict:
+    new = dict()
+
+    def clean_item(item) -> object:
+        # Use this to detect something.
+        # if isinstance(x, tuple):
+        #     raise Exception
+        if not isinstance(item, list):
+            try:
+                return np.asscalar(item)
+            except AttributeError:
+                return item
+        return [
+            clean_item(x)
+            for x in item
+        ]
+
+    for (key, val) in d.items():
+        if isinstance(key, str):
+            new[val] = key
+            continue
+        new[key] = clean_item(val)
+
+    return new
+        
