be0bc426a09c92fa2ab3e2d10fe52481074e2c1c
MasterDoo <masterdu9951@icloud.com>
2019-05-19 17:31:09 -0400

    add model

---
+++ pytorch/FcModel.py
@@ -0,0 +1,30 @@
+"""
+May 19. 2019
+Fully-connected ANN for a baseline model of forecasting
+"""
+from typing import Set
+
+import torch
+import torch.nn.functional as F
+
+
+class FcNet(torch.nn.Module):
+    def __init__(
+        self,
+        num_fea: int,
+        num_tar: int,
+        neurons: Set[int]
+        ):
+        super().__init__()
+        self.fc1 = torch.nn.Linear(num_fea, neurons[0])
+        self.fc2 = torch.nn.Linear(neurons[0], neurons[1])
+        self.fc3 = torch.nn.Linear(neurons[1], neurons[2])
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+if __name__ == "__main__":
+    pass
