86b7868307e5d3f19bf99a6ee03d9e3711509f18
EC2 Server <i@TianyuDu.com>
2019-05-26 01:22:15 +0000

    update

--- pytorch/main_lstm.py
+++ pytorch/main_lstm.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import torch
 import tqdm
+import matplotlib
+matplotlib.use("agg")
 from matplotlib import pyplot as plt
 from tensorboardX import SummaryWriter
 
@@ -17,9 +19,10 @@ plt.style.use("seaborn-dark")
 # import ptc.data_proc as data_proc
 # from ptc.model import *
 
-
+# Default directories for data
 CPIAUCSUL_DATA = "/Users/tianyudu/Documents/Academics/EconForecasting/AnnEconForecast/data/CPIAUCSL.csv"
 SUNSPOT_DATA = "/Users/tianyudu/Documents/Academics/EconForecasting/AnnEconForecast/data/sunspots.csv"
+SUNSPOT_DATA = "/home/ec2-user/environment/AnnEconForecast/data/sunspots.csv"
 
 PROFILE = {
     "TRAIN_SIZE": 231,  # Include both training and validation sets.
