147b9eba02fe693a127bcc280e7a4730017eff0b
MasterDoo <masterdu9951@icloud.com>
2019-05-26 00:11:11 -0400

    update

--- pytorch/test.py
+++ pytorch/test.py
@@ -1,23 +1,38 @@
+"""
+TODO: write documents
+"""
+from pprint import pprint
+from datetime import datetime
+
+import tqdm
+
+import main_lstm
+from core.tools.param_set_generator import gen_hparam_set
+
 CPIAUCSUL_DATA = "/Users/tianyudu/Documents/Academics/EconForecasting/AnnEconForecast/data/CPIAUCSL.csv"
 SUNSPOT_DATA_E = "/home/ec2-user/environment/AnnEconForecast/data/sunspots.csv"
 SUNSPOT_DATA = "/Users/tianyudu/Documents/Academics/EconForecasting/AnnEconForecast/data/sunspots.csv"
 
-PROFILE = {
+SRC_PROFILE = {
     "TRAIN_SIZE": 231,  # Include both training and validation sets.
     "TEST_SIZE": 58,
-    "LAGS": 6,
+    "LAGS": [6, 9],
     "VAL_RATIO": 0.2,  # Validation ratio.
-    "LEARNING_RATE": 0.01,
-    "NEURONS": (32, 128),
-    "EPOCHS": 20,
+    "LEARNING_RATE": [0.01, 0.03],
+    "NEURONS": [(32, 64), (64, 128)],
+    "EPOCHS": 100,
     "LOG_NAME": "untitled",
     "TASK_NAME": "LastOut LSTM on sunspot",
     "DATA_DIR": SUNSPOT_DATA
 }
 
-
-import main_lstm
 if __name__ == "__main__":
-    # globals().update(PROFILE)
-    main_lstm.core(**PROFILE, profile_record=PROFILE)
-
+    profile_set = gen_hparam_set(SRC_PROFILE)
+    print("====Sample Configuration====")
+    pprint(profile_set[0])
+    print("============================")
+    start = datetime.now()
+    for i in tqdm.trange(len(profile_set), desc="Hyper-Param Profile"):
+        PROFILE = profile_set[i]
+        main_lstm.core(**PROFILE, profile_record=PROFILE, verbose=False)
+    print(f"Total time taken: {datetime.now() - start}")
