a6a4494ca6d57634e5f12c9d43a4b154b80c088b
MasterDoo <masterdu9951@icloud.com>
2019-05-25 23:55:35 -0400

    update progress bar message

--- pytorch/main_lstm.py
+++ pytorch/main_lstm.py
@@ -115,7 +115,7 @@ def core(
                 writer.add_scalars(
                     "loss/rmse", {"Validation": func(val_loss)}, i)
             prg.set_description(
-                f"Epoch [{i+1}/{EPOCHS}]: TrainLoss={np.mean(train_loss): 0.3f}, ValLoss={np.mean(val_loss): 0.3f}")
+                f"TrainLoss={np.mean(train_loss): 0.3f}, ValLoss={np.mean(val_loss): 0.3f}")
             # print(f"Epoch: {i}\tTotal Loss: {train_loss:0.6f}\tLatest Val Loss: {val_loss:0.6f}")
         # TODO: deal with the add graph function here.
         # writer.add_graph(net, (torch.zeros(32, LAGS)))
