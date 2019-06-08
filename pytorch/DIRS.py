616055d07f52cfdd57245a100e0eb87ed6ee9c3e
ec2 <i@tianyudu.com>
2019-06-08 20:18:07 +0000

    add DIRS

---
+++ pytorch/DIRS.py
@@ -0,0 +1,14 @@
+# This file stores the directories of datasets on different hosts
+
+CPIAUCSUL = {
+    "mac": "/Users/tianyudu/Documents/Academics/EconForecasting/AnnEconForecast/data/CPIAUCSL.csv",
+    "ec2_cpu": None,
+    "ec2_gpu": None
+    }
+
+SUNSPOT = {
+    "mac": "/Users/tianyudu/Documents/Academics/EconForecasting/AnnEconForecast/data/sunspots.csv",
+    "ec2_cpu": "/home/ec2-user/environment/AnnEconForecast/data/sunspots.csv",
+    "ec2_gpu": "/home/ec2-user/AnnEconForecast/data/sunspots.csv"
+    }
+
