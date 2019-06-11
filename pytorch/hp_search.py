ef102febbb9c5df77a6e28e9df5c5a5180393a17
ec2 <i@tianyudu.com>
2019-06-11 18:12:09 +0000

    upload records

--- pytorch/hp_search.py
+++ pytorch/hp_search.py
@@ -26,9 +26,9 @@ SRC_PROFILE = {
     "TEST_SIZE": 0.2,
     "LAGS": [12, 36],
     "VAL_RATIO": 0.2,  # Validation ratio.
-    "LEARNING_RATE": [0.01, 0.03, 0.1, 0.3],
-    "NEURONS": [(512, 1024), (1024, 2048)],
-    "EPOCHS": [300, 500, 1000],
+    "LEARNING_RATE": [0.03, 0.1, 0.3],
+    "NEURONS": [(128, 126), (256, 512)],
+    "EPOCHS": [300, 500],
     "LOG_NAME": "lastout",
     "TASK_NAME": "Exchange rate",
     "DATA_DIR": DIRS.DEXCAUS["ec2_gpu"]
