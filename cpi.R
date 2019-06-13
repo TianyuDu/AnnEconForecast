3c5ac403e8c9d51017c6f9f6c9ead28c1694d083
MasterDoo <masterdu9951@icloud.com>
2019-06-13 15:08:17 -0400

    update R code for CPI forecasting

--- cpi.R
+++ cpi.R
@@ -1,40 +1,46 @@
 # May 16 2019
+# Modify: Jun. 13 2019
 library(fpp2)
 library(ggfortify)
 library(forecast)
 library(stats)
 library(aTSA)
+library(lmtest)
+library(xts)
+library(anytime)
 
 df <- read.csv("./data/CPIAUCSL.csv", header=TRUE, sep=",")
 
-ts_data <- ts(df$CPIAUCSL,start=c(1947,1),frequency=12)
+# ts_data <- ts(df$CPIAUCSL,start=c(1947,1),frequency=12)
+ts_all <- xts(x=df[c("CPIAUCSL")], order.by=anytime(df$DATE))
 
-autoplot(ts_data) +
+autoplot(ts_all) +
     ggtitle("Consumer Price Index for All Urban Consumers: All Items (CPIAUCSL)") +
     xlab("Date") +
     ylab("CPI") +
     theme(plot.title = element_text(size=8, face="bold"))
 
-# Classical decomposition of target series.
-components.ts <- decompose(ts_data)
-plot(components.ts)
+# Train and test spliting
+train_size <- as.integer(0.8 * length(ts_all))
+test_size <- length(ts_all) - train_size
 
-# The seasonal plot
-ggseasonplot(ts_data,polar=TRUE)+
-    ggtitle("CPIAUCSL (Season plot)")+
-    theme(plot.title = element_text(size=8, face="bold"))
+ts_train <- head(ts_all, train_size)
+ts_test <- tail(ts_all, test_size)
 
-transformed <- diff(ts, lag=1, differences=1)
+# Forecasting of naive preddictor, as a baseline model.
+baseline_error <- mean(
+    na.omit(diff(ts_test)) ** 2
+)
+cat("Base line MSE: ", baseline_error)
 
-ggsubseriesplot(ts_data)+
-    ggtitle("CPIAUCSL (Subseries plot)")+
-    ylab("number of deaths")+
-    theme(plot.title = element_text(size=8, face="bold"))
+# Operations on the Training Set
+# Determine coefficient of integration.
+cat("Order of Integration: ", ndiffs(ts_train))
+transformed <- diff(ts_train, lag=1, differences=2)
 
 ACF <- Acf(transformed, plot=TRUE)
 PACF <-Pacf(transformed, plot=TRUE)
 
-# AR=4, MA=Inf -> ARIMA(4,1,0)
 
 autoplot(transformed) +
     ggtitle("Differenced Series") +
@@ -43,9 +49,49 @@ autoplot(transformed) +
 
 adf.test(transformed)
 
+# Find the Best ARIMA Profile
+auto.arima(ts_train$CPIAUCSL, max.p=10, max.P=10, max.q=10, max.Q=10, max.d=10, max.D=10, stationary=FALSE, trace=TRUE)
+
 # ==== Fit and Evaluate the Model ====
-model <- arima(ts, order = c(4,1,0))
+profile <- c(2, 2, 2)
+model <- arima(ts_train, order=profile)
 res <- residuals(model)
-mse <- mean(res**2)
-print(mse)
-fore <- predict(model, n.ahead=10)
+mse_train <- mean(res**2)
+rmse_train <- sqrt(mse_train)
+
+err_abspec <- abs(res) / as.matrix(ts_train)
+mape_train <- mean(
+    err_abspec[err_abspec != Inf]
+)
+cat("Training Set MSE:", mse_train, "RMSE:", rmse_train, "MAPE:", mape_train)
+
+coeftest(model)
+
+# Simple forecast
+fore <- forecast::forecast(model, h=1, level=c(99))
+autoplot(fore)
+
+# Rolling forecast
+rf <- c()
+for (i in c(1: test_size)){
+    # Refit model
+    model <- arima(
+        head(ts_all, train_size+i-1),
+        # To predict the first element of the test set, we use the whole training set.
+        # To predict the k-th element of the test set (i.e. train_size+k th element), use all previouse train_size+k-1 elements.
+        order=profile
+    )
+    # Produce one step forecast
+    f <- forecast::forecast(model, h=1, level=c(99))
+    rf <- c(rf, f$mean)  # Add rolling forecast.
+}
+ts_rf <- xts(x=rf, order.by=anytime(time(ts_test)))
+combined <- cbind("Rolling Forecast"=ts_rf, "Actual"=ts_test)
+autoplot(combined, facets=FALSE, alpha=0.6) +
+    ggtitle("One Step Rolling Forecasting on Test Set") +
+    xlab("Date") +
+    ylab("Sunspot")
+# Performance Metrics
+mse_test <- mean((ts_rf - ts_test) ** 2)
+rmse_test <- sqrt(mse_test)
+cat("Test MSE:", mse_test, "RMSE:", rmse_test)
