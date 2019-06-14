54ab2f0d4246bf758ec74f8be5c6a6bcbfe5f6b0
ec2 <i@tianyudu.com>
2019-06-14 22:30:41 +0000

    add profile loader

--- pytorch/ProfileLoader.py
+++ pytorch/ProfileLoader.py
@@ -1,14 +1,40 @@
 import json
 import os
-from typing import List
+from typing import List, Union
+import warnings
 
 
 class ProfileLoader():
-    def __init__(self, path: str) -> None:
+    def __init__(self, path: str, verbose: bool=False) -> None:
+        assert os.path.exists(path), "Path does not exist."
+        if not path.endswith("/"):
+            path += "/"
         self.path = path
+        self.verbose = verbose
+        self.all_profiles = [
+            f for f in os.listdir(self.path)
+            if f.endswith(".json")
+        ]
+        assert len(self.all_profiles), "No profile found."
+        self.c = 0
     
-    def get_next_profile(self) -> dict:
-        pass
+    def get_next(self) -> Union[dict, None]:
+        try:
+            with open(self.path + self.all_profiles[self.c], "r") as f:
+                data = json.load(f)
+            self.c += 1
+            return data
+        except IndexError:
+            warnings.warn("All profile has been returned, call ProfileLoader.reset()")
+            return None
         
     def get_all(self) -> List[dict]:
-        pass
+        total = list()
+        for name in self.all_profiles:
+            with open(self.path + name, "r") as f:
+                d = json.load(f)
+            total.append(d)
+        return total
+    
+    def reset(self) -> None:
+        self.c = 0
