c3251296e14f3e84e96f6ea102a7851fe40a01bf
MasterDoo <masterdu9951@icloud.com>
2019-06-17 00:37:59 -0400

    add support

--- pytorch/nnar_controls.py
+++ pytorch/nnar_controls.py
@@ -62,68 +62,130 @@ def core(
     except AssertionError:
         print(f"Default name: {LOG_NAME} is used.")
 
+
+    # ======== Prepare the Dataset ========
     df = raw_df.copy()
+
+    if TRAIN_SIZE < 1 and TEST_SIZE:
+        assert TRAIN_SIZE + TEST_SIZE == 1
+        TRAIN_SIZE = int(len(df) * TRAIN_SIZE)
+        TEST_SIZE = len(df) - TRAIN_SIZE
     
     df_train, df_test = df[:TRAIN_SIZE], df[-TEST_SIZE:]
-    print(f"Train&validation size: {len(df_train)}, test size: {len(df_test)}")
+    
+    if verbose:
+        print(f"Training set: {TRAIN_SIZE}; test set: {TEST_SIZE}")
 
-    gen = SlpGenerator.SlpGenerator(df_train, verbose=False)
+    gen = SlpGenerator.SlpGenerator(df_train, verbose=verbose)
     fea, tar = gen.get_many_to_one(lag=LAGS)
     train_dl, val_dl, train_ds, val_ds = gen.get_tensors(
-        mode="Nto1", lag=LAGS, shuffle=True, batch_size=32, validation_ratio=VAL_RATIO
+        mode="Nto1", lag=LAGS, shuffle=True, batch_size=BATCH_SIZE, validation_ratio=VAL_RATIO
     )
 
-    net = FcModel.Net(num_fea=LAGS, num_tar=1, neurons=NEURONS)
-    optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
-    criterion = torch.nn.MSELoss()
-    # train_log = LogUtility.Logger()
-    # val_log = LogUtility.Logger()
+    if verbose:
+        print(f"Training Set @ {len(train_ds)}\nValidation Set: @ {len(val_ds)}")
 
-    with tqdm.trange(EPOCHS) as prg, SummaryWriter(comment=LOG_NAME) as writer:
+    # ==== Move everything to GPU (if avaiable) ====
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    if verbose:
+        print("Device selected: ", device)
+    
+    if device.type == "cuda":
+        torch.set_default_tensor_type("torch.cuda.FloatTensor")
+
+    # ======== Build the Model ========
+    net = FcModel.Net(num_fea=LAGS, num_tar=1, neurons=NEURONS)
+    net.float()  # Cast all floating point parameters and buffers to double datatype
+    net = net.to(device)
+    
+    optimizer = torch.optim.Adam(net.parameters(), lr=LEARNING_RATE)
+    criterion = torch.nn.MSELoss()
+
+    with tqdm.trange(EPOCHS) as prg, SummaryWriter(comment=NAME) as writer:
         for i in prg:
+            # ======== Training Phase ========
+            if verbose:
+                print(f"======== Training Phase @ Epoch {i} ========")
             train_loss = []
             for batch_idx, (data, target) in enumerate(train_dl):
-                data, target = map(torch.Tensor, (data, target))
+                # Move 
+                data = data.to(device).float()
+                target = target.to(device).float()
+                
                 optimizer.zero_grad()
                 out = net(data)
                 loss = criterion(out, target)
                 train_loss.append(loss.data.item())
                 loss.backward()
                 optimizer.step()
-            # train_log.add(i, np.mean(train_loss))
-            # Write MSE
-            writer.add_scalars(
-                "loss/mse", {"Train": np.mean(train_loss)}, i)
-            # Write RMSE
-
-            def func(x): return np.sqrt(np.mean(x))
-            writer.add_scalars(
-                "loss/rmse", {"Train": func(train_loss)}, i)
-            if i % 5 == 0:
+                # raise Exception("Debug")
+            # MSE Loss
+            writer.add_scalars("loss/mse", {"Train": np.mean(train_loss)}, i)
+            # RMSE Loss
+            _rmse = lambda x: np.sqrt(np.mean(x))
+            writer.add_scalars("loss/rmse", {"Train": _rmse(train_loss)}, i)
+            
+            # ======== Validation Phase ========
+            if verbose:
+                print(f"======== Validation Phase @ Epoch {i} ========")
+            if i % 10 == 0:
                 val_loss = []
                 with torch.no_grad():
                     for batch_idx, (data, target) in enumerate(val_dl):
-                        data, target = map(torch.Tensor, (data, target))
+                        data = data.to(device).float()
+                        target = target.to(device).float()
                         out = net(data)
                         loss = criterion(out, target)
                         val_loss.append(loss.data.item())
-                # val_log.add(i, np.mean(val_loss))
                 writer.add_scalars(
                     "loss/mse", {"Validation": np.mean(val_loss)}, i)
-
                 writer.add_scalars(
-                    "loss/rmse", {"Validation": func(val_loss)}, i)
+                    "loss/rmse", {"Validation": _rmse(val_loss)}, i)
             prg.set_description(
-                f"Epoch [{i+1}/{EPOCHS}]: TrainLoss={np.mean(train_loss): 0.3f}, ValLoss={np.mean(val_loss): 0.3f}")
-        writer.add_graph(net, (torch.zeros(LAGS)))
-        with open(writer.logdir + "/profile.json", "a") as f:
-            encoded = json.dumps(PROFILE)
+                f"TrainMSE:{np.mean(train_loss): 0.7f}, ValMSE:{np.mean(val_loss): 0.7f}")
+        
+        # Save the training profile.
+        with open("./" + writer.logdir + "/profile.json", "a") as f:
+            encoded = json.dumps(profile_record)
             f.write(encoded)
 
-    # Create plot
-    # plt.close()
-    # plt.plot(train_log.get_df(ln=True), label="Train Loss")
-    # plt.plot(val_log.get_df(ln=True), label="Validation Loss")
-    # plt.legend()
-    # plt.grid()
-    # plt.show()
+        # Begin to predict on test set, no need to track gradient here.
+        # ======== Testing Phase ========
+        if verbose:
+            print("======== Testing Phase ========")
+        with torch.no_grad():
+            gen_test = SlpGenerator.SlpGenerator(df_test, verbose=False)
+            fea_df, tar_df = gen_test.get_many_to_one(lag=LAGS)
+            assert len(fea_df) == len(tar_df)
+
+            pred = list()
+            for i in range(len(fea_df)):
+                f = lambda x, idx: torch.Tensor(x.iloc[idx].values)
+                feature = f(fea_df, i)
+                feature = feature.view(1, feature.shape[0])
+                # TODO: check this.
+                # feature = feature.double()
+                # feature = feature.to(device).float()
+                feature = feature.to(device)
+                pred.append(net(feature))
+            
+            pred_df = pd.DataFrame(
+                data=np.array(pred),
+                index=tar_df.index
+            )
+        # ==== visualize test set prediction ====
+            rcParams["lines.linewidth"] = 0.2
+            total = pd.concat([tar_df, pred_df], axis=1)
+            total.columns = ["Actual", "Forecast"]
+            mse = np.mean((total["Actual"] - total["Forecast"])**2)
+            fig = plt.figure(dpi=700)
+
+            plt.plot(total)
+            plt.grid()
+            plt.title(f"Test Set of {NAME} After {EPOCHS} Epochs")
+            plt.xlabel("Date")
+            plt.ylabel("Value")
+            plt.legend(["Actual", f"Forecast, MSE={mse}"])
+            writer.add_figure(
+                "Test set predictions", fig, global_step=EPOCHS
+            )
